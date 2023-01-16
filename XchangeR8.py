3914cd32185b6e5c9f0b486f3527fa84f524fdaa
menisadi <menisadi@gmail.com>
2023-01-16 14:46:53 +0200

    Styling

--- XchangeR8.py
+++ XchangeR8.py
@@ -8,8 +8,10 @@ class Rates:
     """    
     def __init__(self, csv_file):
         """
-        Initialize the class by reading the CSV file, creating a pivot table of the best exchange rates
-        for each currency pair, and creating a directed graph of the rates using NetworkX library.
+        Initialize the class by reading the CSV file, 
+        creating a pivot table of the best exchange rates
+        for each currency pair, 
+        and creating a directed graph of the rates using NetworkX library.
         
         Parameters
         ----------
@@ -24,7 +26,8 @@ class Rates:
         rates : pandas DataFrame
             Pivot table of the best exchange rates for each currency pair.
         rates_graph : networkx.DiGraph
-            Directed graph of the exchange rates, with the weights on the edges representing the 
+            Directed graph of the exchange rates, 
+            with the weights on the edges representing the 
             negative logarithm of the rates.
         """
         self.banks_table = pd.read_csv(csv_file)
@@ -33,39 +36,48 @@ class Rates:
 
     def _update_table_and_graph(self):
         """
-        Update the banks_table, rates, and rates_graph attributes based on the current data in the banks_table.
+        Update the banks_table, rates, and rates_graph attributes 
+        based on the current data in the banks_table.
         
         Attributes
         ----------
         banks_table : pandas DataFrame
             DataFrame containing the exchange rates data, with additional columns 
-            for the best rate and best bank for each currency pair. The banks_table attribute will be updated
-            with the new exchange rates, updating the 'best_bank' and 'best_rate' columns
+            for the best rate and best bank for each currency pair. 
+            The banks_table attribute will be updated
+            with the new exchange rates, 
+            updating the 'best_bank' and 'best_rate' columns
         rates : pandas DataFrame
-            Pivot table of the best exchange rates for each currency pair. This attribute will be recreated 
+            Pivot table of the best exchange rates for each currency pair. 
+            This attribute will be recreated 
             based on the updated banks_table attribute
         rates_graph : networkx.DiGraph
-            Directed graph of the exchange rates, with the weights on the edges representing the 
-            negative logarithm of the rates. This attribute will be recreated based on the updated 
+            Directed graph of the exchange rates, 
+            with the weights on the edges representing the 
+            negative logarithm of the rates. 
+            This attribute will be recreated based on the updated 
             banks_table attribute
         """        
         # Add a column with best rate for each currency pair
         # Drops the "best_rate" column since it shouldn't be considered
-        # Ignore error - in the initial run there is no "best_rate" column so we simply ignore the drop call
+        # Ignore error - in the initial run there is no "best_rate" column 
+        # so we simply ignore the drop call
         self.banks_table['best_rate'] = self.banks_table.select_dtypes(np.number).drop(
             'best_rate', axis=1, errors='ignore').max(axis=1)
         # Add a column with the bank which offers the best rate for each currency pair
         self.banks_table['best_bank'] = self.banks_table.select_dtypes(np.number).drop(
             'best_rate', axis=1, errors='ignore').idxmax(axis=1)
 
-        # Create a pivot table from the best rates so we can use it as an adjacency matrix
+        # Create a pivot table from the best rates so we can use it 
+        # as an adjacency matrix
         self.rates = pd.pivot_table(self.banks_table, index='source', 
             columns='target', values='best_rate')
 
         log_of_rates = -np.log(self.rates)
 
         # Create the exchange graph
-        self.rates_graph = nx.from_pandas_adjacency(log_of_rates, create_using=nx.DiGraph())
+        self.rates_graph = nx.from_pandas_adjacency(log_of_rates, 
+            create_using=nx.DiGraph())
         
     def update_graph(self, updates):
         """
@@ -77,9 +89,13 @@ class Rates:
             A list of dictionaries containing the keys 'from', 'to', 'bank', and 'rate'.
         """        
         for update in updates:
-            source, target, bank, new_rate = update['from'], update['to'], update['bank'], update['rate']
+            source = update['from']
+            target = update['to']
+            bank = update['bank']
+            new_rate = update['rate']
 
-            # Assign the new rate in the corresponding row (source and target) and column (bank to be updated)
+            # Assign the new rate in the corresponding row (source and target) 
+            # and column (bank to be updated)
             self.banks_table.loc[(self.banks_table['source']==source) 
                 & (self.banks_table['target']==target) , bank] = new_rate
             
@@ -90,7 +106,8 @@ class Rates:
     
     def _path_to_list_of_dicts(self, path):
         """
-        Helper method to convert the path returned by the bellman_ford algorithm to a list of dictionaries.
+        Helper method to convert the path returned by the bellman_ford algorithm 
+        to a list of dictionaries.
         
         Parameters
         ----------
@@ -100,7 +117,8 @@ class Rates:
         Returns
         -------
         list of dict
-            A list of dictionaries, where each dictionary has the keys 'from', 'to', and 'bank'.
+            A list of dictionaries, where each dictionary has the keys 
+            'from', 'to', and 'bank'.
         """        
         list_of_dicts = [{"from": currency, "to": next_currency, 
             "bank": self.banks_table.loc[(self.banks_table['source']==currency) 
@@ -110,7 +128,8 @@ class Rates:
 
     def max_revenue(self, query_dict):
         """
-        Find the best path for converting a given amount of currency from one type to another according to the current rates.
+        Find the best path for converting a given amount of currency 
+        from one type to another according to the current rates.
         
         Parameters
         ----------
@@ -120,13 +139,18 @@ class Rates:
         Returns
         -------
         list of dict
-            A list of dictionaries, where each dictionary has the keys 'from', 'to', and 'bank'.
+            A list of dictionaries, where each dictionary has the keys 
+            'from', 'to', and 'bank'.
         """        
-        source, target, amount = query_dict['from'], query_dict['to'], query_dict['amount']
+        source = query_dict['from']
+        target = query_dict['to']
+        amount = query_dict['amount']
+
         try:
             value, path = nx.single_source_bellman_ford(self.rates_graph, source, target)
         except nx.NetworkXNoPath:
-            # In the case that there is no path from source to target we should return an empty string
+            # In the case that there is no path from source to target 
+            # we should return an empty string
             return ""
         except nx.NetworkXUnbounded:
             # We where told that the data won't contain any endless cycles, 
